57759159552197a5f1390e347d937ecd2d1085fc
daniele <daniele@diesel-mac.lan>
2017-06-09 00:04:30 +0200

    Carthage

---
+++ ScrollingStackContainer/ScrollingStackContainer/ScrollingStackContainer.h
@@ -0,0 +1,19 @@
+//
+//  ScrollingStackContainer.h
+//  ScrollingStackContainer
+//
+//  Created by daniele on 09/06/2017.
+//  Copyright © 2017 danielemargutti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScrollingStackContainer.
+FOUNDATION_EXPORT double ScrollingStackContainerVersionNumber;
+
+//! Project version string for ScrollingStackContainer.
+FOUNDATION_EXPORT const unsigned char ScrollingStackContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollingStackContainer/PublicHeader.h>
+
+
